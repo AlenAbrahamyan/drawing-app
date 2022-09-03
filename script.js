f4721cc37c34e61015f3508df0b259f87fd55453
Alen Abrahamyan <alenabrahamyan0@gmail.com>
2022-09-03 16:28:25 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const clearButton = document.getElementById("clear")
+const colorInput = document.getElementById("color")
+const lineWidthInput = document.getElementById("lineWidth")
+
+const canvas = document.getElementById("drawing-board")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 800
+canvas.height = 500
+
+let lineWidth = lineWidthInput.value
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+colorInput.addEventListener("change", (e) => {
+  ctx.strokeStyle = e.target.value
+})
+
+lineWidthInput.addEventListener("change", (e) => {
+  lineWidth = e.target.value
+})
+
+const draw = (e) => {
+  ctx.lineWidth = lineWidth
+  ctx.lineCap = "round"
+
+  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.stroke()
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  ctx.beginPath()
+  canvas.addEventListener("mousemove", draw)
+})
+
+document.addEventListener("mouseup", (e) => {
+  canvas.removeEventListener("mousemove", draw)
+})
