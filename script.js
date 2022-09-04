e7f0184a6388d208ec98b7f7f450a44560603edf
Alen Abrahamyan <alenabrahamyan0@gmail.com>
2022-09-04 14:12:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-const clearButton = document.getElementById("clear")
-const colorInput = document.getElementById("color")
-const lineWidthInput = document.getElementById("lineWidth")
-//
 const canvas = document.getElementById("drawing-board")
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext('2d')
 
 canvas.width = 800
 canvas.height = 500
 
-let lineWidth = lineWidthInput.value
+ctx.fillStyle = 'white'
+ctx.fillRect(0,0, canvas.width, canvas.height)
 
-clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+const clearBtn = document.getElementById('clear')
+const colorInput = document.getElementById('color')
+const lineWidthInput = document.getElementById('line-width')
+
+clearBtn.addEventListener('click', ()=>{
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
 })
 
-colorInput.addEventListener("change", (e) => {
-  ctx.strokeStyle = e.target.value
+colorInput.addEventListener('change', ()=>{
+  ctx.strokeStyle = colorInput.value
 })
 
-lineWidthInput.addEventListener("change", (e) => {
-  lineWidth = e.target.value
+lineWidthInput.addEventListener('change', ()=>{
+  ctx.lineWidth = lineWidthInput.value
 })
 
 const draw = (e) => {
-  ctx.lineWidth = lineWidth
-  ctx.lineCap = "round"
-
   ctx.lineTo(e.offsetX, e.offsetY)
   ctx.stroke()
 }
 
-canvas.addEventListener("mousedown", (e) => {
+canvas.addEventListener('mousedown', ()=>{
   ctx.beginPath()
-  canvas.addEventListener("mousemove", draw)
+  canvas.addEventListener('mousemove', draw)
 })
 
-document.addEventListener("mouseup", (e) => {
-  canvas.removeEventListener("mousemove", draw)
+window.addEventListener('mouseup', ()=>{
+  canvas.removeEventListener('mousemove', draw)
+  ctx.closePath()
 })
